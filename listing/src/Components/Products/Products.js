847be11e79c5aef255b453ca26bd7ae105e1dd45
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-12-04 20:43:07 +0530

    feat: added feature and sort

--- listing/src/Components/Products/Products.js
+++ listing/src/Components/Products/Products.js
@@ -1,29 +1,119 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import "./Products.css";
+import List from "../../data.json"
+
 
 function Products() {
   const [data, setData] = useState([]);
+  const [brand, setBrand] =useState([]);
+  const [sort, setSort] =useState("");
+  const [size, setSize] = useState([]);
+  const [suitable, setSuitable]= useState([]);
+  const [sortedData, setSortedData] = useState([]);
+  const [isSorted, setisSorted]= useState(false);
+  
 
+  const clearAll = ()=>{
+    setSize([]);
+    setBrand([]);
+    setSuitable([]);
+    setSort("")
+  }
+
+  
+ 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=20")
-      .then((res) => res.json())
-      .then((json) => setData(json));
-  }, []);
+    if(sort === "High"){
+      const sortedDatabyHigh = (data.sort(function (a, b) {
+        return b.price - a.price;
+      }));
+      setSortedData(sortedDatabyHigh);
+      setData(sortedData);
+    }else if(sort === "Low"){
+      const sortedDatabyLow = (data.sort(function (a, b) {
+        return a.price - b.price;
+      }));
+      setSortedData(sortedDatabyLow)
+      setData(setSortedData);
+    }else if(sortedData === ""){
+      setData(List)
+    }
+    setData(List);
+    if(size.length > 0){
+     setData(  data.filter((i) => size.includes(i.size[0])))
+    }
+    if(brand.length > 0){
+     setData(  data.filter((i) => brand.includes(i.brand)))
+    }
+    if(suitable.length > 0){
+      setData(data.filter((i)=> suitable.includes(i.Suitable)))
+    }
+  }, [sort,brand, size, suitable, isSorted, sortedData, data]);
 
   return (
     <div className="main">
       <div className="Filter">
         <div>
+        <button onClick={clearAll}>Clear All</button>
+        <div>
+        Sort Products By:
+        <div>
+        <button onClick={()=>{setSort("High"); setisSorted(true)}}>High to Low</button>
+        <button onClick={()=>{setSort("Low"); setisSorted(true)}}>Low to High</button>
+        {isSorted && <button onClick={()=> window.location.reload()}>Clear Sort</button>}
+        </div>
+        </div>
           <h5>
             Filters
-            <button className="clear">Clear all</button>
+           
           </h5>
           <div>
             <div>Size</div>
-            {data.map((item) => (
-              <li type="checkbox">{item.price}</li>
-            ))}
+            {[...new Set( data?.map((products)=>products.size[0]))].map(
+              (option) => (
+                <div>
+                  <button
+                    type="checkbox"
+                    id="size"
+                    name="size"
+                    value="size"
+                    onClick={() => {setSize([...size, option])}}>{option}
+                  </button>
+                  <label for="size"></label>
+                </div>
+              )
+            )}
+          </div>
+           <div>
+            <div>Brand</div>
+            {[...new Set( data?.map((products)=>products.brand))].map(
+              (option) => (
+                <div>
+                  <button
+                    type="checkbox"
+                    id="brand"
+                    name="brand"
+                    value="brand"
+                    onClick={() => {setBrand([...brand, option])}}>{option}
+                  </button>
+                  <label for="brand"></label>
+                </div>
+              )
+            )}
+          </div>
+          <div>
+            <div>Suitable for</div>
+            {[...new Set( data?.map((products)=>products.Suitable))].map(
+              (option) => (
+                <div>
+                  <button
+                    onClick={() => {setSuitable([...suitable, option])}}>{option}
+                  </button>
+                  <label for="suitable"></label>
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
